c737c293cc2ff91566484a3bb3f72c9be653965e
Nurahmadie <nurahmadie@gmail.com>
2013-09-15 12:58:59 +0700

    Allow http to https redirection, and add user-agent.
    Some site actually enable HSTS, support this via `open_uri_redirections` gem.
    
    Also add default user agent and introduce ENV["FEEDBAG_UA"]
    to override this.

--- feedbag.gemspec
+++ feedbag.gemspec
@@ -1,11 +1,11 @@
 # -*- encoding: utf-8 -*-
- 
+
 Gem::Specification.new do |s|
   s.name = %q{feedbag}
   s.version = "0.9.1"
   s.homepage = "http://axiombox.com/feedbag"
   s.rubyforge_project = "feedbag"
- 
+
   s.authors = ["Axiombox", "David Moreno", "Derek Willis"]
   s.date = %q{2012-03-16}
   s.description = %q{Ruby's favorite feed auto-discoverty tool}
@@ -15,9 +15,12 @@ Gem::Specification.new do |s|
   s.has_rdoc = true
   s.rdoc_options = ["--main", "README.markdown"]
   s.summary = %q{Ruby's favorite feed auto-discovery tool}
-  s.add_dependency("nokogiri") 
-  s.add_development_dependency "shoulda" 
-  s.add_development_dependency "mocha", "~> 0.12.0"
+
+  s.add_dependency("nokogiri")
+  s.add_dependency("open_uri_redirections")
+
+  s.add_development_dependency "shoulda"
+  s.add_development_dependency "mocha", ">= 0.12.0"
   s.bindir = 'bin'
   s.default_executable = %q{feedbag}
   s.executables = ["feedbag"]
--- lib/feedbag.rb
+++ lib/feedbag.rb
@@ -24,18 +24,21 @@
 require "rubygems"
 require "nokogiri"
 require "open-uri"
+require "open_uri_redirections"
 require "net/http"
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36"
 
 class Feedbag
 
-	CONTENT_TYPES = [
-		'application/x.atom+xml',
-		'application/atom+xml',
-		'application/xml',
-		'text/xml',
-		'application/rss+xml',
-		'application/rdf+xml',
-	].freeze
+  CONTENT_TYPES = [
+    'application/x.atom+xml',
+    'application/atom+xml',
+    'application/xml',
+    'text/xml',
+    'application/rss+xml',
+    'application/rdf+xml',
+  ].freeze
 
   def self.feed?(url)
     new.feed?(url)
@@ -49,160 +52,161 @@ class Feedbag
     @feeds = []
   end
 
-	def feed?(url)
-		# use LWR::Simple.normalize some time
-		url_uri = URI.parse(url)
-		url = "#{url_uri.scheme or 'http'}://#{url_uri.host}#{url_uri.path}"
-		url << "?#{url_uri.query}" if url_uri.query
-		
-		# hack:
-		url.sub!(/^feed:\/\//, 'http://')
+  def feed?(url)
+    # use LWR::Simple.normalize some time
+    url_uri = URI.parse(url)
+    url = "#{url_uri.scheme or 'http'}://#{url_uri.host}#{url_uri.path}"
+    url << "?#{url_uri.query}" if url_uri.query
 
-		res = Feedbag.find(url)
-		if res.size == 1 and res.first == url
-			return true
-		else
-			return false
-		end
-	end
+    # hack:
+    url.sub!(/^feed:\/\//, 'http://')
 
-	def find(url, args = {})
-		url_uri = URI.parse(url)
-		url = nil
-		if url_uri.scheme.nil?
-		  url = "http://#{url_uri.to_s}"
-		elsif url_uri.scheme == "feed"
-		  return self.add_feed(url_uri.to_s.sub(/^feed:\/\//, 'http://'), nil)
-		else
-		  url = url_uri.to_s
-		end
-		#url = "#{url_uri.scheme or 'http'}://#{url_uri.host}#{url_uri.path}"
+    res = Feedbag.find(url)
+    if res.size == 1 and res.first == url
+      return true
+    else
+      return false
+    end
+  end
 
-		# check if feed_valid is avail
+  def find(url, args = {})
+    url_uri = URI.parse(url)
+    url = nil
+    if url_uri.scheme.nil?
+      url = "http://#{url_uri.to_s}"
+    elsif url_uri.scheme == "feed"
+      return self.add_feed(url_uri.to_s.sub(/^feed:\/\//, 'http://'), nil)
+    else
+      url = url_uri.to_s
+    end
+    #url = "#{url_uri.scheme or 'http'}://#{url_uri.host}#{url_uri.path}"
+
+    # check if feed_valid is avail
     begin
-  		require "feed_validator"
-	  	v = W3C::FeedValidator.new
-		  v.validate_url(url)
-			return self.add_feed(url, nil) if v.valid?
-  	rescue LoadError
-	  	# scoo
-		rescue REXML::ParseException
-  	  # usually indicates timeout
-	    # TODO: actually find out timeout. use Terminator?
-	    # $stderr.puts "Feed looked like feed but might not have passed validation or timed out"
+      require "feed_validator"
+      v = W3C::FeedValidator.new
+      v.validate_url(url)
+      return self.add_feed(url, nil) if v.valid?
+    rescue LoadError
+      # scoo
+    rescue REXML::ParseException
+      # usually indicates timeout
+      # TODO: actually find out timeout. use Terminator?
+      # $stderr.puts "Feed looked like feed but might not have passed validation or timed out"
     rescue => ex
-  		$stderr.puts "#{ex.class} error ocurred with: `#{url}': #{ex.message}"
-	  end
+      $stderr.puts "#{ex.class} error ocurred with: `#{url}': #{ex.message}"
+    end
 
-		begin
-			html = open(url) do |f|
-				content_type = f.content_type.downcase
-				if content_type == "application/octet-stream" # open failed
-				  content_type = f.meta["content-type"].gsub(/;.*$/, '')
-				end
-				if CONTENT_TYPES.include?(content_type)
-					return self.add_feed(url, nil)
-				end
+    begin
+      user_agent = ENV["FEEDBAG_UA"] || USER_AGENT
+      html = open(url, "User-Agent" => user_agent, :allow_redirections => :safe) do |f|
+        content_type = f.content_type.downcase
+        if content_type == "application/octet-stream" # open failed
+          content_type = f.meta["content-type"].gsub(/;.*$/, '')
+        end
+        if CONTENT_TYPES.include?(content_type)
+          return self.add_feed(url, nil)
+        end
 
-				doc = Nokogiri::HTML(f.read)
+        doc = Nokogiri::HTML(f.read)
 
-				if doc.at("base") and doc.at("base")["href"]
-					@base_uri = doc.at("base")["href"]
-				else
-					@base_uri = nil
-				end
+        if doc.at("base") and doc.at("base")["href"]
+          @base_uri = doc.at("base")["href"]
+        else
+          @base_uri = nil
+        end
 
-				# first with links
+        # first with links
         (doc/"atom:link").each do |l|
-					next unless l["rel"]
-					if l["type"] and CONTENT_TYPES.include?(l["type"].downcase.strip) and l["rel"].downcase == "self"
-						self.add_feed(l["href"], url, @base_uri)
-					end
-				end
+          next unless l["rel"]
+          if l["type"] and CONTENT_TYPES.include?(l["type"].downcase.strip) and l["rel"].downcase == "self"
+            self.add_feed(l["href"], url, @base_uri)
+          end
+        end
 
-				(doc/"link").each do |l|
-					next unless l["rel"]
-					if l["type"] and CONTENT_TYPES.include?(l["type"].downcase.strip) and (l["rel"].downcase =~ /alternate/i or l["rel"] == "service.feed")
-						self.add_feed(l["href"], url, @base_uri)
-					end
-				end
+        (doc/"link").each do |l|
+          next unless l["rel"]
+          if l["type"] and CONTENT_TYPES.include?(l["type"].downcase.strip) and (l["rel"].downcase =~ /alternate/i or l["rel"] == "service.feed")
+            self.add_feed(l["href"], url, @base_uri)
+          end
+        end
 
-				(doc/"a").each do |a|
-  				next unless a["href"]
-	  			if self.looks_like_feed?(a["href"]) and (a["href"] =~ /\// or a["href"] =~ /#{url_uri.host}/)
-		  			self.add_feed(a["href"], url, @base_uri)
-			  	end
-				end
+        (doc/"a").each do |a|
+          next unless a["href"]
+          if self.looks_like_feed?(a["href"]) and (a["href"] =~ /\// or a["href"] =~ /#{url_uri.host}/)
+            self.add_feed(a["href"], url, @base_uri)
+          end
+        end
 
-  			(doc/"a").each do |a|
-	  			next unless a["href"]
-		  		if self.looks_like_feed?(a["href"])
-			  		self.add_feed(a["href"], url, @base_uri)
-				  end
-				end
+        (doc/"a").each do |a|
+          next unless a["href"]
+          if self.looks_like_feed?(a["href"])
+            self.add_feed(a["href"], url, @base_uri)
+          end
+        end
 
         # Added support for feeds like http://tabtimes.com/tbfeed/mashable/full.xml
         if url.match(/.xml$/) and doc.root and doc.root["xml:base"] and doc.root["xml:base"].strip == url.strip
-					self.add_feed(url, nil)
+          self.add_feed(url, nil)
         end
-			end
-		rescue Timeout::Error => err
-			$stderr.puts "Timeout error ocurred with `#{url}: #{err}'"
-		rescue OpenURI::HTTPError => the_error
-			$stderr.puts "Error ocurred with `#{url}': #{the_error}"
-		rescue SocketError => err
-			$stderr.puts "Socket error ocurred with: `#{url}': #{err}"
-		rescue => ex
-			$stderr.puts "#{ex.class} error ocurred with: `#{url}': #{ex.message}"
-		ensure
-			return @feeds
-		end
-		
-	end
+      end
+    rescue Timeout::Error => err
+      $stderr.puts "Timeout error ocurred with `#{url}: #{err}'"
+    rescue OpenURI::HTTPError => the_error
+      $stderr.puts "Error ocurred with `#{url}': #{the_error}"
+    rescue SocketError => err
+      $stderr.puts "Socket error ocurred with: `#{url}': #{err}"
+    rescue => ex
+      $stderr.puts "#{ex.class} error ocurred with: `#{url}': #{ex.message}"
+    ensure
+      return @feeds
+    end
 
-	def looks_like_feed?(url)
-		if url =~ /(\.(rdf|xml|rdf|rss)$|feed=(rss|atom)|(atom|feed)\/?$)/i
-			true
-		else
-			false
-		end
-	end
+  end
 
-	def add_feed(feed_url, orig_url, base_uri = nil)
-		# puts "#{feed_url} - #{orig_url}"
-		url = feed_url.sub(/^feed:/, '').strip
+  def looks_like_feed?(url)
+    if url =~ /(\.(rdf|xml|rdf|rss)$|feed=(rss|atom)|(atom|feed)\/?$)/i
+      true
+    else
+      false
+    end
+  end
 
-		if base_uri
-			#	url = base_uri + feed_url
-			url = URI.parse(base_uri).merge(feed_url).to_s
-		end
+  def add_feed(feed_url, orig_url, base_uri = nil)
+    # $stderr.puts "#{feed_url} - #{orig_url}\n"
+    url = feed_url.sub(/^feed:/, '').strip
 
-		begin
-			uri = URI.parse(url)
-		rescue
-			puts "Error with `#{url}'"
-			exit 1
-		end
-		unless uri.absolute?
-			orig = URI.parse(orig_url)
-			url = orig.merge(url).to_s
-		end
+    if base_uri
+      #	url = base_uri + feed_url
+      url = URI.parse(base_uri).merge(feed_url).to_s
+    end
 
-		# verify url is really valid
-		@feeds.push(url) unless @feeds.include?(url)# if self._is_http_valid(URI.parse(url), orig_url)
-	end
+    begin
+      uri = URI.parse(url)
+    rescue
+      puts "Error with `#{url}'"
+      exit 1
+    end
+    unless uri.absolute?
+      orig = URI.parse(orig_url)
+      url = orig.merge(url).to_s
+    end
 
-	# not used. yet.
-	def _is_http_valid(uri, orig_url)
-		req = Net::HTTP.get_response(uri)
-		orig_uri = URI.parse(orig_url)
-		case req
-			when Net::HTTPSuccess then
-				return true
-			else
-				return false
-		end
-	end
+    # verify url is really valid
+    @feeds.push(url) unless @feeds.include?(url)# if self._is_http_valid(URI.parse(url), orig_url)
+  end
+
+  # not used. yet.
+  def _is_http_valid(uri, orig_url)
+    req = Net::HTTP.get_response(uri)
+    orig_uri = URI.parse(orig_url)
+    case req
+    when Net::HTTPSuccess then
+      return true
+    else
+      return false
+    end
+  end
 end
 
 if __FILE__ == $0
