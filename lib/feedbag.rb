44cd9f8b2e83c7988ce50563de1c68ae24dc691d
Nurahmadie <nurahmadie@gmail.com>
2016-02-07 04:46:33 +0700

    Require kconv

--- lib/feedbag.rb
+++ lib/feedbag.rb
@@ -26,6 +26,7 @@ require "nokogiri"
 require "open-uri"
 require "open_uri_redirections"
 require "net/http"
+require "kconv"
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36"
 
